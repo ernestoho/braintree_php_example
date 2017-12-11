a73b1ad12f4f59ae746c766dfc665bc7ec7b0b95
ernestoho <junior.ho@gmail.com>
2017-12-11 19:09:49 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,7 @@
 <?php require_once("../includes/braintree_init.php"); ?>
 
 <html>
-<?php require_once("../includes/head.php"); ?>
-<body>
-
-    <?php require_once("../includes/header.php"); ?>
-
-    <div class="wrapper">
-        <div class="checkout container">
-
-            <head>
+<head>
   <meta charset="utf-8">
   <script src="https://js.braintreegateway.com/web/dropin/1.9.2/js/dropin.min.js"></script>
 </head>
