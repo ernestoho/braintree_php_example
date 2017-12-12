d19d07753efd05dde0bed6e5cb7f9d5d941ba76f
ernestoho <junior.ho@gmail.com>
2017-12-12 00:35:34 -0400

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BraintreePHPExample</title>
     <link rel=stylesheet type=text/css href="css/app.css">
     <link rel=stylesheet type=text/css href="css/overrides.css">
